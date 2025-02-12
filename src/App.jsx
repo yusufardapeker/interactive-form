12a8540a04744f08549806bd7e97e8a2cc71716c
Yusuf Arda Peker <yusufardapkr@gmail.com>
2025-02-12 14:12:30 +0300

    Code general mobile layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import "./styles/reset.css";
 import "./styles/global.scss";
 
+import { Card } from "./components/Card";
+import { Form } from "./components/Form";
+import { Completed } from "./components/Completed";
+
+import bgMainMobile from "./images/bg-main-mobile.png";
+import bgMainDesktop from "./images/bg-main-desktop.png";
+
 function App() {
 	return (
 		<>
-			<h1>Hello!</h1>
+			<picture>
+				<source media="(min-width: 1440px)" srcSet={bgMainDesktop} />
+				<img src={bgMainMobile} />
+			</picture>
+
+			<Card />
+
+			<div className="actions-wrapper">
+				<Form />
+				<Completed />
+			</div>
 		</>
 	);
 }
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import "./card.scss";
+
+import cardBackImg from "../../images/bg-card-back.png";
+import cardFrontImg from "../../images/bg-card-front.png";
 
 function Card() {
-	return <div>Card</div>;
+	return (
+		<div>
+			<div className="card-back-wrapper">
+				<img src={cardBackImg} />
+			</div>
+			<div className="card-front-wrapper">
+				<img src={cardFrontImg} />
+			</div>
+		</div>
+	);
 }
 
 export { Card };

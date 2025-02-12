76e4db61e4a53f5285955a9c4713d54635efcf92
Yusuf Arda Peker <yusufardapkr@gmail.com>
2025-02-12 14:28:51 +0300

    Code card content

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,14 +4,32 @@ import "./card.scss";
 import cardBackImg from "../../images/bg-card-back.png";
 import cardFrontImg from "../../images/bg-card-front.png";
 
+import cardLogo from "../../images/card-logo.svg";
+
 function Card() {
 	return (
 		<div>
 			<div className="card-back-wrapper">
 				<img src={cardBackImg} />
+
+				<span className="cvc-number">000</span>
 			</div>
+
 			<div className="card-front-wrapper">
 				<img src={cardFrontImg} />
+
+				<div className="content">
+					<img src={cardLogo} className="card-logo" />
+
+					<div className="card-information">
+						<p className="card-number">0000 0000 0000 0000</p>
+
+						<div className="name-date-wrapper">
+							<p className="name">Jane Appleseed</p>
+							<span className="date">00/00</span>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
